bcb2aef66860eca15c370c6f5a72dbe985a85ec2
seebreeze35 <seebreeze35@gmail.com>
2014-02-02 22:25:36 -0500

    Opponent now generates a program to pick a random valid move. Bug in valid moves function

--- opponent.py
+++ opponent.py
@@ -6,43 +6,66 @@ class opponent:
     def __init__(self, color):
         self.id = 0
         self.color = color
-        self.program = None
+        self.pieceProgram = None
+        self.moveProgram = None
         self.genPieceProgram()
-        print self.program.display()
+        self.genMoveProgram()
+#        print self.pieceProgram.display()
+        print self.moveProgram.display()
 
     #need to generate a move program
     #get a list of valid moves from the give piece
     #create the move from the moce program
 
-    def move(self, validMoves, board):
-        moveMax = None
-        m = None
+    def move(self, validPieces, board):
+        pieceMax, moveMax = None, None
+        p, m = None, None
+
+        for valid in validPieces:
+           # print str(valid.x)  +' '+str(valid.y)
+            num = self.pieceProgram.evaluate([valid.x, valid.y])
+            if num >pieceMax or pieceMax == None:
+                pieceMax = num
+                p = valid
+
+        validMoves = board.getPieceMoves(p)
+
+#        print 'moves'
+ #       for valid in validMoves:
+  #          print str(valid.mX)+' '+str(valid.mY)
 
         for valid in validMoves:
            # print str(valid.x)  +' '+str(valid.y)
-            num = self.program.evaluate([valid.x, valid.y])
-           # print num
+            num = self.moveProgram.evaluate([valid.mX, valid.mY])
             if num >moveMax or moveMax == None:
                 moveMax = num
                 m = valid
-
-        validMoves = board.getPieceMoves(m)
 
         print 'moves--'
         for valid in validMoves:
             print str(valid.mX)+' '+str(valid.mY)
 
-        print 'Piece: '+str(m.x)+' '+str(m.y)
+        print 'Piece: '+str(p.x)+' '+str(p.y)
+        print 'Move: '+str(m.mX)+' '+str(m.mY)
 
-        _move = move()
-        _move.inX = m.x
-        _move.inY = m.y
-        _move.inputMove()
-        return _move
+#        _move = move()
+ #       _move.inX = m.x
+  #      _move.inY = m.y
+   #     _move.inputMove()
+        return m
 
     def genPieceProgram(self):
         size = False
         while (size == False):
             program = ai.makerandomtree(2)
             size = ai.getSize(program)
-        self.program = program
+        self.pieceProgram = program
+
+    def genMoveProgram(self):
+        size = False
+        while (size == False):
+            program = ai.makerandomtree(2)
+            size = ai.getSize(program)
+        self.moveProgram = program
+
+    

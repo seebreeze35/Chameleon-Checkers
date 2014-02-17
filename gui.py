e29b56bfd894e57575a44cb8546bb23dca4dad3a
seebreeze35 <seebreeze35@gmail.com>
2014-02-16 22:10:59 -0500

    Added initial menubar to gui

--- gui.py
+++ gui.py
@@ -8,6 +8,9 @@ class Application(Frame):
     def train(self):
         subprocess.call("./train.py")
 
+    def hello(self):
+        print 'hello'
+
     def createWidgets(self):
 
         self.parent.title("Checkers")
@@ -17,10 +20,10 @@ class Application(Frame):
   #      self.rowconfigure(3, weight=1)
    #     self.rowconfigure(5, pad=7)
 
-        self.hi = Button(self)
-        self.hi['text'] = 'Train'
-        self.hi['command'] = self.train
-        self.hi.grid(row=1, column=2)
+        self.TRAIN = Button(self)
+        self.TRAIN['text'] = 'Train'
+        self.TRAIN['command'] = self.train
+        self.TRAIN.grid(row=1, column=2)
 
         self.QUIT = Button(self)
         self.QUIT['text'] = "Quit"
@@ -28,14 +31,21 @@ class Application(Frame):
         self.QUIT['command'] = self.quit
         self.QUIT.grid(row=3, column=2)
 
+        self.menubar = Menu(self.parent)
+        filemenu = Menu(self.menubar, tearoff=0)
+        filemenu.add_command(label = 'Test', command=self.hello)
+        self.menubar.add_cascade(label="File", menu=filemenu)
+
+        self.parent.config(menu=self.menubar)
+
         self.canvas = Canvas(self, width=300, height=250)
         self.canvas.grid(row=1, column=0, columnspan=2, rowspan=4, padx=5, sticky=E+W+S+N)
-        self.canvas.create_rectangle(50, 20, 150, 80, fill="#476042")
-        self.canvas.create_rectangle(65, 35, 135, 65, fill="yellow")
-        self.canvas.create_line(0, 0, 50, 20, fill="#476042", width=3)
-        self.canvas.create_line(0, 100, 50, 80, fill="#476042", width=3)
-        self.canvas.create_line(150,20, 200, 0, fill="#476042", width=3)
-        self.canvas.create_line(150, 80, 200, 100, fill="#476042", width=3)
+#        self.canvas.create_rectangle(50, 20, 150, 80, fill="#476042")
+ #       self.canvas.create_rectangle(65, 35, 135, 65, fill="yellow")
+  #      self.canvas.create_line(0, 0, 50, 20, fill="#476042", width=3)
+   #     self.canvas.create_line(0, 100, 50, 80, fill="#476042", width=3)
+    #    self.canvas.create_line(150,20, 200, 0, fill="#476042", width=3)
+     #   self.canvas.create_line(150, 80, 200, 100, fill="#476042", width=3)
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
